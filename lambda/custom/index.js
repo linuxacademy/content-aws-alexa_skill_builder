c82acbec609d7b55a0495c30230c0b265c293a34
lfritts <lfritts@protonmail.com>
2019-11-13 11:44:56 -0800

    ready for video

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -57,6 +57,61 @@ const FairyGodmotherIntentHandler = {
   },
 };
 
+const InProgressMakeAnimalIntent = {
+  canHandle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+
+    return request.type === 'IntentRequest'
+      && request.intent.name === 'MakeAnimalIntent'
+      && request.dialogState !== 'COMPLETED';
+  },
+  handle(handlerInput) {
+    const currentIntent = handlerInput.requestEnvelope.request.intent;
+    let prompt = '';
+
+    for (const slotName of Object.keys(handlerInput.requestEnvelope.request.intent.slots)) {
+      const currentSlot = currentIntent.slots[slotName];
+      if (currentSlot.confirmationStatus !== 'CONFIRMED'
+                && currentSlot.resolutions
+                && currentSlot.resolutions.resolutionsPerAuthority[0]) {
+        if (currentSlot.resolutions.resolutionsPerAuthority[0].status.code === 'ER_SUCCESS_MATCH') {
+          if (currentSlot.resolutions.resolutionsPerAuthority[0].values.length > 1) {
+            prompt = 'Which would you like';
+            const size = currentSlot.resolutions.resolutionsPerAuthority[0].values.length;
+
+            currentSlot.resolutions.resolutionsPerAuthority[0].values
+              .forEach((element, index) => {
+                prompt += ` ${(index === size - 1) ? ' or' : ' '} ${element.value.name}`;
+              });
+
+            prompt += '?';
+
+            return handlerInput.responseBuilder
+              .speak(prompt)
+              .reprompt(prompt)
+              .addElicitSlotDirective(currentSlot.name)
+              .getResponse();
+          }
+        } else if (currentSlot.resolutions.resolutionsPerAuthority[0].status.code === 'ER_SUCCESS_NO_MATCH') {
+          if (requiredSlots.indexOf(currentSlot.name) > -1) {
+            prompt = `What ${currentSlot.name} are you looking for`;
+
+            return handlerInput.responseBuilder
+              .speak(prompt)
+              .reprompt(prompt)
+              .addElicitSlotDirective(currentSlot.name)
+              .getResponse();
+          }
+        }
+      }
+    }
+
+    return handlerInput.responseBuilder
+      .addDelegateDirective(currentIntent)
+      .getResponse();
+  },
+};
+
 // Make Animal
 // Turns the user into animal
 const MakeAnimalIntentHandler = {
@@ -149,7 +204,9 @@ exports.handler = skillBuilder
     // IntentReflectorHandler,
     WelcomeIntentHandler,
     FairyGodmotherIntentHandler,
-    MakeAnimalIntentHandler
+    // MakeAnimalIntentHandler,
+    InProgressMakeAnimalIntent
+
   )
   .addErrorHandlers(ErrorHandler)
   .lambda();
